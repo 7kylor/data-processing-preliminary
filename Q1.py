e1f0c7ea6f861febe8da1959ab67c719f018aaa9
7kylor <20871728+7kylor@users.noreply.github.com>
2025-09-26 15:44:38 +0400

    first commit

---
+++ Q1.py
@@ -0,0 +1,6 @@
+# Summation of two numbers
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+# print the sum of the two numbers
+print( f"The sum of {a} and {b} is = {a + b}")
